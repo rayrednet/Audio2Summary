158dde7fda83f26e0d6cecf6f6c3cddce3298082
unknown <rayssa.sibarani@gmail.com>
2025-03-04 00:34:08 +0700

    feat: add test file to check gpu

---
+++ test.py
@@ -0,0 +1,4 @@
+import torch
+print(torch.cuda.is_available())  # Should print True
+print(torch.cuda.device_count())  # Should print number of GPUs
+print(torch.cuda.get_device_name(0))  # Should print GPU name
